bab83304cae6dd8bad5a21cbdfd77c7e3bd1d616
Joost Diepenmaat <joost@zeekat.nl>
2009-03-31 12:43:28 +0200

    added copyright

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-/home/joost/lib/flex/bin/mxmlc -load-config /home/joost/lib/flex/frameworks/air-config.xml GenerateAudio.as
+#!/bin/sh
+
+# I'm using air-config.xml because that has the right triggers
+# for flash 10 compilation.
+
+$HOME/lib/flex/bin/mxmlc -load-config $HOME/lib/flex/frameworks/air-config.xml GenerateAudio.as
+
